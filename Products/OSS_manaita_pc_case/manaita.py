be0ad9afb58c3b1d4c6902957d22211239ca2315
bacon <bacon0817.ix@gmail.com>
2023-11-14 13:59:07 +0900

    yaml対応

--- Products/OSS_manaita_pc_case/manaita.py
+++ Products/OSS_manaita_pc_case/manaita.py
@@ -1,14 +1,16 @@
 from build123d import *
 from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
-import settings
+import yaml
 
 BOARD_THIN=5
 
+with open('Products\OSS_manaita_pc_case\settings.yaml', encoding='utf-8') as file:
+    settings=yaml.safe_load(file)
+
 #板作成
-main_board=Box(settings.ATX['X'],settings.ATX['Y'],BOARD_THIN)
+main_board=Box(settings['ATX']['X'],settings['ATX']['Y'],BOARD_THIN)
 #板を左下が原点になるように移動
-main_board=Pos(settings.ATX['X']/2,settings.ATX['Y']/2)*main_board
-print(settings.ATX['HOLE'][0])
-for i,item in enumerate(settings.ATX['HOLE']):
-    main_board-=Pos(settings.ATX['HOLE'][i]['x'],settings.ATX['HOLE'][i]['y'])*Cylinder(settings.ATX['HOLE_DIA'] / 2, height=BOARD_THIN)
+main_board=Pos(settings['ATX']['X']/2,settings['ATX']['Y']/2)*main_board
+for i,item in enumerate(settings['ATX']['HOLE']):
+    main_board-=Pos(settings['ATX']['HOLE'][i]['x'],settings['ATX']['HOLE'][i]['y'])*Cylinder(settings['ATX']['HOLE_DIA'] / 2, height=BOARD_THIN)
 show_object(main_board)
