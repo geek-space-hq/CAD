b4012897a1a1c259ef026f42a017f46d8d8ccd14
bacon <bacon0817.ix@gmail.com>
2023-11-16 17:25:02 +0900

    コメント追加

--- Products/OSS_manaita_pc_case/manaita.py
+++ Products/OSS_manaita_pc_case/manaita.py
@@ -21,21 +21,32 @@ def make_board(name):
 
 def make_hole(work,name,offset_x,offset_y):
     #ワークに穴を開ける
+    #ボードの形状を表示する用のポリライン
     board=[
-        (0,0,BOARD_THIN),
-        (settings[name]['X'],0,BOARD_THIN),
-        (settings[name]['X'],settings[name]['Y'],BOARD_THIN),
-        (0,settings[name]['Y'],BOARD_THIN),
-        (0,0,BOARD_THIN)
+        (0+offset_x,0+offset_y,BOARD_THIN),
+        (settings[name]['X']+offset_x,0+offset_y,BOARD_THIN),
+        (settings[name]['X']+offset_x,settings[name]['Y']+offset_y,BOARD_THIN),
+        (0+offset_x,settings[name]['Y']+offset_y,BOARD_THIN),
+        (0+offset_x,0+offset_y,BOARD_THIN)
         ]
     l1=Polyline(*board)
+    
+    #穴あけ形状判定
     if settings[name]['HOLE']!=None:
         for i,item in enumerate(settings[name]['HOLE']):
+            #オフセット分ずらして渡されたworkに穴あけ
             work-=Pos(settings[name]['HOLE'][i]['x']+offset_x,settings[name]['HOLE'][i]['y']+offset_y)*Cylinder(settings[name]['HOLE_DIA'] / 2, height=BOARD_THIN)
     return [work,l1]
 
 atx_board=make_board('A3_BOARD')
-atx_board=make_hole(atx_board,'ATX',0,0)
+
+atx_board=make_hole(
+    work=atx_board,
+    name='ATX',
+    offset_x=0,
+    offset_y=settings['A3_BOARD']['Y']-settings['ATX']['Y']
+    )
+
 show_object(atx_board)
 
 #2.5インチSSD
