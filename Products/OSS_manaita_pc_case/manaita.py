07907222b814e4b8942467d226a3bf074bc18b7e
bacon <bacon0817.ix@gmail.com>
2023-11-16 15:00:59 +0900

    ボード位置表示用ポリライン追加

--- Products/OSS_manaita_pc_case/manaita.py
+++ Products/OSS_manaita_pc_case/manaita.py
@@ -14,12 +14,28 @@ def make_board(name):
     main_board=Box(settings[name]['X'],settings[name]['Y'],BOARD_THIN)
     #板を左下が原点になるように移動
     main_board=Pos(settings[name]['X']/2,settings[name]['Y']/2)*main_board
-    for i,item in enumerate(settings[name]['HOLE']):
-        main_board-=Pos(settings[name]['HOLE'][i]['x'],settings[name]['HOLE'][i]['y'])*Cylinder(settings[name]['HOLE_DIA'] / 2, height=BOARD_THIN)
+    if settings[name]['HOLE']!=None:
+        for i,item in enumerate(settings[name]['HOLE']):
+            main_board-=Pos(settings[name]['HOLE'][i]['x'],settings[name]['HOLE'][i]['y'])*Cylinder(settings[name]['HOLE_DIA'] / 2, height=BOARD_THIN)
     return main_board
 
-atx_board=make_board('SATA2.5inc_V')
+def make_hole(work,name,offset_x,offset_y):
+    #ワークに穴を開ける
+    board=[
+        (0,0,BOARD_THIN),
+        (settings[name]['X'],0,BOARD_THIN),
+        (settings[name]['X'],settings[name]['Y'],BOARD_THIN),
+        (0,settings[name]['Y'],BOARD_THIN),
+        (0,0,BOARD_THIN)
+        ]
+    l1=Polyline(*board)
+    if settings[name]['HOLE']!=None:
+        for i,item in enumerate(settings[name]['HOLE']):
+            work-=Pos(settings[name]['HOLE'][i]['x']+offset_x,settings[name]['HOLE'][i]['y']+offset_y)*Cylinder(settings[name]['HOLE_DIA'] / 2, height=BOARD_THIN)
+    return [work,l1]
 
+atx_board=make_board('A3_BOARD')
+atx_board=make_hole(atx_board,'ATX',0,0)
 show_object(atx_board)
 
 #2.5インチSSD
